da6778a1834896b32f0b617b509e954e5721e80c
asosunoff <al.sosunoff@gmail.com>
2020-04-03 00:19:50 +0300

    fix

--- module-2/task_8.js
+++ module-2/task_8.js
@@ -1,3 +1,42 @@
-const trimSymbols = 11;
+const trimSymbols = (str, count) => {
+	if (!count) return "";
 
-trimSymbols('xxxaaaaab', 1); // 'xab'
+	let result = "";
+	let c = 0;
+	let r = "";
+
+	str.split("").forEach(char => {
+		debugger;
+
+		let lastChar = r[r.length - 1];
+
+		if (!lastChar) {
+			r += char;
+		} else {
+			if (lastChar != char) {
+				c = 0;
+				r = char;
+				++c;
+			}
+
+			// if (c == 0) {
+			//     result += char;
+			//     ++c;
+			// }
+
+			if (lastChar == char && c < count) {
+				r += char;
+				++c;
+			}
+
+			if (r.length == count) {
+				result += r;
+				r = "";
+			}
+		}
+	});
+
+	return result;
+};
+
+console.log(trimSymbols("xxxaaaaab", 1)); // 'xab'
