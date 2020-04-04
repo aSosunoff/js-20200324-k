c61671b3d165e62f207c40873eb9eff95ac878fc
asosunoff <al.sosunoff@gmail.com>
2020-04-04 16:17:21 +0300

    solve notification

--- module-3/notification/notification.js
+++ module-3/notification/notification.js
@@ -1,3 +1,64 @@
-export default function NotificationMessage(message, obj) {
+function createElementFromHTML(htmlString) {
+	var div = document.createElement("div");
+	div.innerHTML = htmlString.trim();
+	return div.firstElementChild;
+}
 
+export default class NotificationMessage {
+	element;
+	duration;
+	type;
+	message;
+	timer = null;
+	get template() {
+		return `<div class="notification ${this.type}" style="--value: ${
+			this.duration / 1000
+		}s;">
+			<div class="timer"></div>
+			<div class="inner-wrapper">
+				<div class="notification-header">${this.type}</div>
+				<div class="notification-body">${this.message}</div>
+			</div>
+		</div>`;
+	}
+	static activeNotice = null;
+
+	constructor(message, { duration = 1000, type = "success" } = {}) {
+		NotificationMessage.activeNotice &&
+			NotificationMessage.activeNotice.remove();
+
+		this.duration = duration;
+		this.type = type;
+		this.message = message;
+		this.render();
+		this.initEventListeners();
+	}
+
+	show(target) {
+		let container = target && "append" in target ? target : document.body;
+
+		container.append(this.element);
+		
+		clearTimeout(this.timer);
+
+		this.timer = setTimeout(this.remove.bind(this), this.duration);
+	}
+
+	initEventListeners() {}
+
+	render() {
+		this.element = createElementFromHTML(this.template);
+		NotificationMessage.activeNotice = this;
+	}
+
+	remove() {
+		this.element.remove();
+		clearTimeout(this.timer);
+		this.timer = null;
+	}
+
+	destroy() {
+		this.remove();
+		this.activeNotice = null;
+	}
 }
