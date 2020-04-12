e2257edd397a86fbc3fdaa465bf3f15d9f3ac648
asosunoff <al.sosunoff@gmail.com>
2020-04-12 11:53:01 +0300

    solve 5 module

--- module-5/tooltip/index.js
+++ module-5/tooltip/index.js
@@ -1,33 +1,84 @@
 class Tooltip {
-  static instance;
+	static instance;
+	
+	options = {};
+	element;
 
-  element;
+	constructor() {
+		if (Tooltip.instance) {
+			return Tooltip.instance;
+		}
 
-  constructor() {
-    if (Tooltip.instance) {
-      return Tooltip.instance;
-    }
+		this.onPointerOver = this.onPointerOver.bind(this);
+		this.onPointerOut = this.onPointerOut.bind(this);
+		this.onPointerMove = this.onPointerMove.bind(this);
 
-    Tooltip.instance = this;
-  }
+		Tooltip.instance = this;
+	}
 
-  initEventListeners () {
+	showTooltip(text) {
+		this.element = document.createElement(this.options.tag);
+		this.element.className = this.options.className;
+		this.element.innerHTML = text;
+		document.body.append(this.element);
+	}
 
-  }
+	onPointerOver(e) {
+		const spaceTooltip = e.target.closest("[data-tooltip]");
+		if (!spaceTooltip) return;
+		this.showTooltip(spaceTooltip.dataset.tooltip);
+		document.addEventListener("pointermove", this.onPointerMove);
+	}
 
-  initialize () {
+	onPointerOut(e) {
+		if (!this.element) return;
+		this.element.remove();
+		this.element = null;
+		document.removeEventListener("pointermove", this.onPointerMove);
+	}
 
-  }
+	onPointerMove(e) {
+		let newX = e.clientX + 10;
+		let newY = e.clientY + 10;
 
-  render () {
+		if (newX + this.element.offsetWidth > document.documentElement.clientWidth) {
+			newX = document.documentElement.clientWidth - this.element.offsetWidth
+		}
 
-  }
+		if (newY + this.element.offsetHeight > document.documentElement.clientHeight) {
+			newY = document.documentElement.clientHeight - this.element.offsetHeight
+		}
 
-  destroy () {
+		this.element.style.left = `${newX}px`;
+		this.element.style.top = `${newY}px`
+	}
 
-  }
+	initEventListeners() {
+		document.addEventListener("pointerover", this.onPointerOver);
+		document.addEventListener("pointerout", this.onPointerOut);
+	}
+
+	initialize({ className = 'tooltip', tag = 'div' } = {}) {
+		this.options = {
+			className,
+			tag,
+		}
+
+		this.initEventListeners();
+	}
+
+	render(text) {
+		this.showTooltip(text);
+	}
+
+	destroy() {
+		if (this.element) {
+			this.element.remove();
+			this.element = null;
+		};
+		document.removeEventListener("pointerover", this.onPointerOver);
+		document.removeEventListener("pointerout", this.onPointerOut);
+	}
 }
 
-const tooltip = new Tooltip();
-
-export default tooltip;
+export default new Tooltip();
--- module-5/tooltip/tooltip.spec.js
+++ module-5/tooltip/tooltip.spec.js
@@ -1,17 +1,69 @@
-import tooltip from "./index";
+import * as module from "./index";
+
+const { default: tooltip } = module;
 
 describe("tooltip", () => {
-  beforeEach(() => {
-    tooltip.initialize();
-  });
+	beforeEach(() => {
+		tooltip.initialize();
+		document.body.append(
+			createElementFromHTML('<div data-tooltip="foo"></div>')
+		);
+	});
 
-  afterEach(() => {
-    tooltip.destroy();
-  });
+	afterEach(() => {
+		tooltip.destroy();
+		document.body.innerHTML = '';
+	});
 
-  it("should be rendered correctly", () => {
-    tooltip.render('');
-    expect(tooltip.element).toBeVisible();
-    expect(tooltip.element).toBeInTheDocument();
-  });
+	it("should be rendered correctly", () => {
+		const text = "Test text";
+		tooltip.render(text);
+		expect(tooltip.element).toBeVisible();
+		expect(tooltip.element).toBeInTheDocument();
+		expect(tooltip.element).toHaveTextContent(text);
+	});
+
+	it("should be rendered by class name 'tooltip'", () => {
+		tooltip.render();
+		expect(tooltip.element.classList.contains("tooltip")).toBeTruthy();
+	});
+
+	it("should be rendered by class name 'tooltip_2'", () => {
+		const className = "tooltip_2";
+		tooltip.initialize({ className });
+		tooltip.render();
+		expect(tooltip.element.classList.contains(className)).toBeTruthy();
+	});
+
+	it("should be visible after event 'pointerover'", () => {
+		const div = document.querySelector("[data-tooltip]");
+		div.dispatchEvent(new MouseEvent("pointerover", { bubbles: true }));
+		expect(document.body).toContainElement(document.querySelector(`.${tooltip.options.className}`));
+		expect(document.querySelector(`.${tooltip.options.className}`)).toBeVisible();
+		expect(document.querySelector(`.${tooltip.options.className}`)).toBeInTheDocument();
+	});
+
+	it("should not be visible after event 'pointerout'", () => {
+		const div = document.querySelector("[data-tooltip]");
+		div.dispatchEvent(new MouseEvent("pointerover", { bubbles: true }));
+		div.dispatchEvent(new MouseEvent("pointerout", { bubbles: true }));
+		expect(document.querySelector(`.${tooltip.options.className}`)).toBeNull();
+		expect(document.querySelector(`.${tooltip.options.className}`)).not.toBeInTheDocument();
+	});
+
+	it("should be contain text 'foo' from data attribute", () => {
+		const div = document.querySelector("[data-tooltip]");
+		div.dispatchEvent(new MouseEvent("pointerover", { bubbles: true }));
+		expect(document.querySelector(`.${tooltip.options.className}`)).toHaveTextContent('foo');
+	});
+
+	it("should be a singleton realization", () => {
+		// ??
+	});
 });
+
+function createElementFromHTML(htmlString) {
+	var div = document.createElement("div");
+	div.innerHTML = htmlString.trim();
+	return div.firstElementChild;
+}
